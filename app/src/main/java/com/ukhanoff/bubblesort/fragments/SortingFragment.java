1f6efa5a0f97bddae196c5b0451c5e5a89c47dce
Maksym Ukhanov <max.ukhanov@gmail.com>
2017-02-02 21:09:15 +0200

    Base BubbleDrawer was implemented. Now parsed values from text converts to list of ImageViews.

--- app/src/main/java/com/ukhanoff/bubblesort/fragments/SortingFragment.java
+++ app/src/main/java/com/ukhanoff/bubblesort/fragments/SortingFragment.java
@@ -1,22 +1,97 @@
 package com.ukhanoff.bubblesort.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.ukhanoff.bubblesort.R;
+import com.ukhanoff.bubblesort.utils.Utils;
+import com.ukhanoff.bubblesort.views.BubbleImageView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ukhanoff on 2/2/17.
  */
 
 public class SortingFragment extends Fragment {
+    public static final int PADDING = 50;
+    public static final int INTERNAL_ID_PREFIX = 100000;
+    private EditText editText;
+    private Button goButton;
+    private LinearLayout bubblesContainer;
+    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String inputUserArray = editText.getText().toString();
+            if (!TextUtils.isEmpty(inputUserArray)) {
+                parseData(inputUserArray);
+            } else {
+                Toast.makeText(getContext(), R.string.empty_field_warning, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sorting_main_fragment_view, container, false);
+        View fragmentView = inflater.inflate(R.layout.sorting_main_fragment_view, container, false);
+        editText = (EditText) fragmentView.findViewById(R.id.array_input);
+        goButton = (Button) fragmentView.findViewById(R.id.go_sort_btn);
+        goButton.setOnClickListener(buttonClickListener);
+        bubblesContainer = (LinearLayout) fragmentView.findViewById(R.id.bubbles_container);
+        return fragmentView;
     }
+
+    private void drawBubbles(ArrayList<Integer> listToDraw) {
+        if (bubblesContainer != null) {
+            bubblesContainer.removeAllViews();
+        }
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        int marginInPx = Utils.dpToPx(getContext(), 4);
+        lp.setMargins(0, 0, marginInPx, 0);
+
+        int pos = 0;
+        for (Integer currentIntValue : listToDraw) {
+            BubbleImageView bubbleImageView = new BubbleImageView(getContext());
+            bubbleImageView.setImageBitmap(createCalculatedBitmap(currentIntValue));
+            bubbleImageView.setNumber(currentIntValue);
+            bubbleImageView.setId(INTERNAL_ID_PREFIX + pos);
+            if (bubblesContainer != null) {
+                bubblesContainer.addView(bubbleImageView, lp);
+            }
+            pos++;
+        }
+    }
+
+    private Bitmap createCalculatedBitmap(Integer currentIntValue) {
+        final Rect bounds = new Rect();
+        Paint paint = new Paint(Paint.LINEAR_TEXT_FLAG);
+        paint.setTextSize(40f);
+        paint.getTextBounds(currentIntValue.toString(), 0, currentIntValue.toString().length(), bounds);
+        return Bitmap.createBitmap(bounds.width() + PADDING, bounds.height() + PADDING, Bitmap.Config.RGB_565);
+    }
+
+    private void parseData(String inputUserArray) {
+        ArrayList<Integer> parsedUserArray = new ArrayList<>();
+        String[] stringArray = inputUserArray.split(",");
+        int numberOfElements = stringArray.length;
+        for (int i = 0; i < numberOfElements; i++) {
+            parsedUserArray.add(Integer.parseInt(stringArray[i]));
+        }
+        drawBubbles(parsedUserArray);
+    }
+
 }
---
+++ app/src/main/java/com/ukhanoff/bubblesort/views/BubbleImageView.java
@@ -0,0 +1,63 @@
+package com.ukhanoff.bubblesort.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.ukhanoff.bubblesort.R;
+
+import static com.ukhanoff.bubblesort.fragments.SortingFragment.PADDING;
+
+/**
+ * This is custom ImageView which could draw a "Bubble with a number inside".
+ */
+
+public class BubbleImageView extends ImageView {
+    public static final int START_X_POS = 25;
+    public static final int TEXT_BASELINE_Y = 55;
+    private Integer valueToDraw;
+
+    public BubbleImageView(Context context) {
+        this(context, null);
+    }
+
+    public BubbleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BubbleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+        if (valueToDraw != null) {
+            String text = valueToDraw.toString();
+            Paint paint = new Paint(Paint.LINEAR_TEXT_FLAG);
+            Rect bounds = new Rect();
+            paint.setAntiAlias(true);
+            paint.setTextSize(40f);
+            paint.getTextBounds(valueToDraw.toString(), 0, valueToDraw.toString().length(), bounds);
+            paint.setColor(getResources().getColor(R.color.colorAccent));
+            canvas.drawOval(0, 0, bounds.width() + PADDING, bounds.height() + PADDING, paint);
+            paint.setColor(Color.BLACK);
+            canvas.drawText(text.toString(), START_X_POS, TEXT_BASELINE_Y, paint);
+        }
+    }
+
+    /**
+     * Draws a number as a bitmap inside of the bubble circle.
+     *
+     * @param numberValueToDraw value which should appears in the center of {@link BubbleImageView}
+     */
+    public void setNumber(Integer numberValueToDraw) {
+        valueToDraw = numberValueToDraw;
+        invalidate();
+    }
+}
